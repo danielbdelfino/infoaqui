137821ec924fae0ca2c06c9913c5f80edcfefcb4
“daniel.delfino” <daniel.delfino@sankhya.com.br>
2021-11-20 04:11:55 -0300

    Ajustes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  plugins: ['gatsby-theme-material-ui', 'gatsby-plugin-use-query-params'],
+  plugins: ['gatsby-theme-material-ui', 'gatsby-plugin-use-query-params',
+  {
+    resolve: `gatsby-plugin-google-adsense`,
+    options: {
+      publisherId: `ca-pub-3197953936610029`
+    },
+  }],
   siteMetadata: {
     title: "Infoaqui",
     titleTemplate: "%s · Informção com qualidade",
     description:
       "InfoAqui - Conteúdo em um só lugar!",
-    url: "http://www.infoaqui.com.br", // No trailing slash allowed!
+    url: "http://www.infoaqui.net.br", // No trailing slash allowed!
     image: "/images/infoaqui.png", // Path to your image you placed in the 'static' folder
     twitterUsername: "@danielbdelfino",
     icon: 'src/images/touchwifi_small.png'
